d54771b842425484a2505635f942e36d26fa8ecb
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2022-07-26 14:10:08 +0000

    Add files via upload

---
+++ analog_date.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from time import *
+
+def update():
+    time_string=strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d ,%Y")
+    date_label.config(text=date_string)
+
+    time_label.after(2000,update)
+    window.after(2000, update)
+
+window=Tk()
+window.title("Digital Clock")
+
+time_label=Label(window,font=("Arial",50),fg="#00ff00",bg="black")
+time_label.pack()
+
+day_label=Label(window,font=("Ink Free",25))
+day_label.pack()
+
+date_label=Label(window,font=("Ink Free",30))
+date_label.pack()
+
+update()
+
+window.mainloop()
